bbc01aba1d6c7c5b08d8a0a4cc03791e8e1df3ce
James Calhoun <jim.calhoun@outlook.com>
2018-02-11 12:22:23 -0500

    update routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,24 @@
 'use strict';
 
-module.exports = function (app, passport) {
+module.exports = function (app) {
 	app.route('/')
 		.get(function (req, res) {
 			res.render('index');
+		});
+	app.route('/stocks')
+		.get(function (req, res) {
+			var stocks = [];
+			// Access database and pull in stock info
+			res.json({ stocks });
+		})
+		.post(function (req, res) {
+			// Look up symbol/company
+			// Look up stock history
+			// Add to database
+			// Trigger socket event
+		})
+		.delete(function (req, res) {
+			// Look up stock in database and delete it
+			// Trigger socket event
 		});
 };
